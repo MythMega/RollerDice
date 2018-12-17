d043478aacc56862fb0e093f130f56569ab3f0f5
MythMega <mythmegass@gmail.com>
2018-12-17 11:26:20 +0100

    Creation Base MainActivity

--- app/src/main/java/com/bts/morinbetend/rollerdicegame/MainActivity.java
+++ app/src/main/java/com/bts/morinbetend/rollerdicegame/MainActivity.java
@@ -1,13 +1,27 @@
 package com.bts.morinbetend.rollerdicegame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnJouer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeIHM();
+        btnJouer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getApplicationContext(), gameActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
+
+    private void initializeIHM() { btnJouer = findViewById(R.id.btn_jouer); }
 }
